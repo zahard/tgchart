c0598df6a0a2aa35849dad314aac5bab6c688d53
Zakhar Portianov <kkragy@gmail.com>
2019-03-10 13:10:46 +0200

    Animation and range scale

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const svg = document.getElementById('svgroot');
 
 function generateData (max) {
   var data = [];
-  for (let i=0; i<45;i++) {
+  for (let i=0; i<63;i++) {
     data.push(randomInteger(5, max));
   }
   return data;
@@ -39,20 +39,45 @@ function drawPath(el, path, color, lineWidth, id) {
   }));
 }
 
+function udpatePath(path, id) {
+  svg.querySelector('#' + id).setAttribute('d', path);
+}
 
-var POINT_OFFSET = 40;
+
+var POINT_OFFSET = 20;
 
 
 
-var data2 = generateData(120);
+//var data2 = generateData(120);
 var data = generateData(300);
 
-var max1Value = Math.max.apply(null, data);
-var max2Value = Math.max.apply(null, data2);
-var totalMax = Math.max(max1Value, max2Value);
+var totalMax = Math.max.apply(null, data);
+//var max2Value = Math.max.apply(null, data2);
+//var totalMax = Math.max(max1Value, max2Value);
 
 drawPath(svg, fitPath(data, totalMax, 400, POINT_OFFSET), '#f34c44', 3, 'data-1');
-drawPath(svg, fitPath(data2,  totalMax, 400, POINT_OFFSET), '#3cc23f', 3, 'data-2');
+//drawPath(svg, fitPath(data2,  totalMax, 400, POINT_OFFSET), '#3cc23f', 3, 'data-2');
+
+var targetData = data.slice(0);
+var animationData = new Array(data.length).fill(0);
+
+ii = 0;
+function animate() {
+  if (ii == 25) {
+    return;
+  }
+  ii++;
+  var len = targetData.length;
+  for (var i =0; i< len; i++) {
+    dx = targetData[i] / 25;
+    animationData[i] += dx;
+  }
+  udpatePath(fitPath(animationData, totalMax, 400, POINT_OFFSET), 'data-1');
+  
+  requestAnimationFrame(() => {
+    animate();
+  });
+}
 
 function fitPath(data, maxValue, h, pointsOffset) {
   var len = data.length;
@@ -70,128 +95,5 @@ function fitPath(data, maxValue, h, pointsOffset) {
 }
 
 
-var pointsOffset = 400 / (data.length - 1);
-const svgPreview= document.getElementById('svg-preview');
-drawPath(svgPreview, fitPath(data, totalMax, 60, pointsOffset), '#f34c44', 2, 'data-preview');
-drawPath(svgPreview, fitPath(data2, totalMax, 60, pointsOffset), '#3cc23f', 2, 'data-preview-2');
 
-
-var pointPerView = 400 / POINT_OFFSET;
-var viewWidth = 100 / (data.length - 1) * pointPerView;
-var viewOffset = 0;
-
-
-const area_f = document.querySelector('.area-focused');
-const area_drag = document.querySelector('.area-focused-drag');
-const area_l = document.querySelector('.area-left');
-const area_r = document.querySelector('.area-right');
-
-
-
-updateRange(viewWidth, viewOffset);
-
-function updateRange(width, offset) {
-  area_l.style.width = offset + '%';
-
-  area_f.style.width = width + '%';
-  area_drag.style.width = width + '%';
-
-  area_f.style.left = offset + '%';
-  area_drag.style.left = offset + '%';
-
-  area_r.style.left = (offset + width) + '%';
-  area_r.style.width = (100 - (offset + width)) + '%';
-}
-
-
-
-
-
-
-
-var container  = document.querySelector('.controls');
-var dragItem  = document.querySelector('.area-focused-drag');
-var expandLeft  = document.querySelector('.area-left .drag-point');
-var expandRight  = document.querySelector('.area-right .drag-point');
-
-var active = false;
-var currentX;
-var initialX;
-var xOffset = 0;
-var initalWidth;
-
-const dragStart = (e) => {
-  if (e.target !== dragItem && e.target !== expandLeft && e.target !== expandRight) {
-    return
-  }
-  
-  initalWidth = viewWidth;
-
-  active = true;
-
-  var xOffset = e.target.offsetLeft;
-
-  if (e.type === "touchstart") {
-    initialX = e.touches[0].clientX - xOffset;
-  } else {
-    initialX = e.clientX - xOffset;
-  }
-}
-
-var initialOffset = 0;
-var containerPoint = container.offsetWidth / 100;
-
-const drag = (e) => {
-  if (!active) {
-    return;
-  }
-  e.preventDefault();
-
-  if (e.type === "touchmove") {
-    currentX = e.touches[0].clientX - initialX;
-  } else {
-    currentX = e.clientX - initialX;    
-  }
-
-  if (e.target === dragItem) {
-      viewOffset = currentX / containerPoint;
-      viewOffset = Math.max(0, viewOffset);
-      viewOffset = Math.min(100 - viewWidth, viewOffset);
-      
-      updateRange(viewWidth, viewOffset);
-      
-      udpateRootOffset(viewOffset);
-  } else {
-    if(e.target === expandRight) {
-      viewWidth = initalWidth + currentX / containerPoint;
-    } else if(e.target === expandLeft) {
-      
-      viewOffset = currentX / containerPoint;
-      viewOffset = Math.max(0, viewOffset);
-      viewOffset = Math.min(100 - viewWidth, viewOffset);
-      viewWidth = initalWidth - currentX / containerPoint;
-    }
-    
-    updateRange(viewWidth, viewOffset);
-  }
-
-}
-
-const dragEnd = (e) => {
-  active = false;
-}
-
-function udpateRootOffset(viewOffset) {  
-  var offset = 400 * viewOffset / viewWidth;
-  svg.setAttribute('viewBox', `${offset} 0 400 400`);
-}
-
-
-container.addEventListener("touchstart", dragStart, false);
-container.addEventListener("touchend", dragEnd, false);
-container.addEventListener("touchmove", drag, false);
-
-container.addEventListener("mousedown", dragStart, false);
-container.addEventListener("mouseup", dragEnd, false);
-container.addEventListener("mousemove", drag, false);
 
---
+++ preview.js
@@ -0,0 +1,146 @@
+
+
+var pointsOffset = 400 / (data.length - 1);
+const svgPreview= document.getElementById('svg-preview');
+drawPath(svgPreview, fitPath(data, totalMax, 60, pointsOffset), '#f34c44', 2, 'data-preview');
+//drawPath(svgPreview, fitPath(data2, totalMax, 60, pointsOffset), '#3cc23f', 2, 'data-preview-2');
+
+
+
+var pointPerView = 400 / POINT_OFFSET;
+var viewWidth = 100 / (data.length - 1) * pointPerView;
+var viewOffset = 0;
+
+
+const area_f = document.querySelector('.area-focused');
+const area_drag = document.querySelector('.area-focused-drag');
+const area_l = document.querySelector('.area-left');
+const area_r = document.querySelector('.area-right');
+
+
+
+updateRange(viewWidth, viewOffset);
+
+function updateRange(width, offset) {
+  area_l.style.width = offset + '%';
+
+  area_f.style.width = width + '%';
+  area_drag.style.width = width + '%';
+
+  area_f.style.left = offset + '%';
+  area_drag.style.left = offset + '%';
+
+  area_r.style.left = (offset + width) + '%';
+  area_r.style.width = (100 - (offset + width)) + '%';
+}
+
+
+var container  = document.querySelector('.controls');
+var dragItem  = document.querySelector('.area-focused-drag');
+var expandLeft  = document.querySelector('.area-left .drag-point');
+var expandRight  = document.querySelector('.area-right .drag-point');
+
+var active = false;
+var currentX;
+var initialX;
+var initalWidth;
+var initialOffset;
+
+const dragStart = (e) => {
+  if (e.target !== dragItem && e.target !== expandLeft && e.target !== expandRight) {
+    return
+  }
+  active = true;
+  initalWidth = viewWidth;
+  initialOffset = viewOffset;
+
+  if (e.type === "touchstart") {
+    initialX = e.touches[0].clientX;
+  } else {
+    initialX = e.clientX;
+  }
+
+  switch (e.target) {
+    case dragItem:
+      onDrag = onFrameDrag;
+      break;
+    case expandLeft:
+      onDrag = onExpandLeft;
+      break;
+      case expandRight:
+      onDrag = onExpandRight;
+      break;
+  }
+}
+
+var containerPoint = container.offsetWidth / 100;
+
+const drag = (e) => {
+  if (!active) {
+    return;
+  }
+  e.preventDefault();
+  if (e.type === "touchmove") {
+    currentX = e.touches[0].clientX - initialX;
+  } else {
+    currentX = e.clientX - initialX;    
+  }
+  onDrag(currentX);
+}
+
+const onFrameDrag = (currentX) => {
+  viewOffset = initialOffset + currentX / containerPoint;
+  viewOffset = Math.max(0, viewOffset);
+  viewOffset = Math.min(100 - viewWidth, viewOffset);
+  updateRange(viewWidth, viewOffset);
+  udpateRootOffset(viewOffset);
+}
+
+const onExpandLeft = () => {  
+  var increment = currentX / containerPoint;
+  viewWidth = initalWidth - increment;
+  
+  //viewWidth = Math.max(viewWidth, 10);
+  //viewWidth = Math.min(100 - viewOffset, viewWidth);
+
+  viewOffset = initialOffset + increment;
+  scalePath();
+
+  updateRange(viewWidth, viewOffset);
+  udpateRootOffset(viewOffset);
+  
+}
+
+const onExpandRight = () => {
+  viewWidth = initalWidth + currentX / containerPoint;
+  viewWidth = Math.max(viewWidth, 10);
+  viewWidth = Math.min(100 - viewOffset, viewWidth);
+
+  scalePath();
+  updateRange(viewWidth, viewOffset);
+  udpateRootOffset(viewOffset);
+}
+
+const dragEnd = (e) => {
+  console.log('DragEnd');
+  active = false;
+}
+function scalePath() {
+  var pointPerView = viewWidth * (data.length - 1) / 100
+  POINT_OFFSET = 400 / pointPerView;
+  udpatePath(fitPath(data, totalMax, 400, POINT_OFFSET), 'data-1');
+}
+
+function udpateRootOffset(viewOffset) {  
+  var offset = 400 * viewOffset / viewWidth;
+  svg.setAttribute('viewBox', `${offset} 0 400 400`);
+}
+
+
+container.addEventListener("touchstart", dragStart, false);
+container.addEventListener("touchend", dragEnd, false);
+container.addEventListener("touchmove", drag, false);
+
+container.addEventListener("mousedown", dragStart, false);
+container.addEventListener("mouseup", dragEnd, false);
+container.addEventListener("mousemove", drag, false);
